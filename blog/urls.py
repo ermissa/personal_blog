b8b23e5b704217c036caafb96cade7bfde6a71fd
ermissa <ermissa@itu.edu.tr>
2018-04-27 02:13:42 +0300

    important changes are made in url.py. See the comments in blog/url.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path,include
 from django.contrib import admin
-
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path(r'^$',views.getPosts,name="bloghome"),
-    path(r'^(?P<selected_page>\d+)/?$',views.getPosts,name="bloghome"),    
-    #path('',views.blogHomePage,name="bloghome"),
-    path('<slug>',views.postDetailPage,name="post_detail")
+    #Regexs are not used in path() function. (This function is New in Django2.0 and the use of this function is encouraged.)#
+    path('',views.getPosts,name="bloghome"),
+    path('<slug>/',views.postDetailPage,name="post_detail")
+
+    #I am learning when I writing this blog so I decided not to delete following lines. These mistakes maybe beneficial for someone.#
+    #! FOR url() FUNCTION : FROM DJANGO DOCUMENTATIONS : "It’s likely to be deprecated in a future release."
+    
+    #url('',views.getPosts,name="bloghome"),
+    #url(r'^(?P<selected_page>\d+)/?$',views.getPosts,name="bloghome"),    
+    #url('^<slug>/?',views.postDetailPage,name="post_detail")
+    #url(r'^(?P<slug>[\w-]+)/$',views.postDetailPage,name="post_detail")
 ]
 
